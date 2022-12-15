b4105b36d45d74c200c635e4546aa671a77ebe9d
HendrixTech <hendrixobuks@gmial.com>
2022-12-15 15:41:40 -0800

    operational function

---
+++ func_1.c
@@ -0,0 +1,133 @@
+#include "monty.h"
+
+/**
+ * swap_stack - swaps the top two elements of the stack
+ * @stack: stack
+ * @line_number: line
+ * Return: void pointer
+ */
+
+void swap_stack(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+
+	if (*stack && (*stack)->next)
+	{
+		tmp = *stack;
+		*stack = (*stack)->next;
+		(*stack)->prev = NULL;
+		tmp->next = (*stack)->next;
+		(*stack)->next = tmp;
+		tmp->prev = *stack;
+	}
+	else
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+}
+
+/**
+ * add_stack - adds the top two elements of the stack
+ * @stack: stack
+ * @line_number: line
+ * Return: void pointer
+ */
+
+void add_stack(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+
+	if (*stack && (*stack)->next)
+	{
+		tmp = *stack;
+		*stack = (*stack)->next;
+		(*stack)->prev = NULL;
+		(*stack)->n += tmp->n;
+		free(tmp);
+	}
+	else
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+}
+
+/**
+ * nop_stack - does nothing
+ * @stack: stack
+ * @line_number: line
+ * Return: void pointer
+ */
+
+void nop_stack(stack_t **stack, unsigned int line_number)
+{
+	if (stack && line_number)
+	{
+	}
+}
+
+/**
+ * sub_stack - substracts the top two elements of the stack
+ * @stack: stack
+ * @line_number: line
+ * Return: void pointer
+ */
+
+void sub_stack(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+	
+	if (*stack && (*stack)->next)
+	{
+		tmp = *stack;
+		*stack = (*stack)->next;
+		(*stack)->prev = NULL;
+		(*stack)->n -= tmp->n;
+		free(tmp);
+	}
+	else
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+}
+
+/**
+ * div_stack - divides the top two elements of the stack
+ * @stack: stack
+ * @line_number: line
+ * Return: void pointer
+ */
+
+void div_stack(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+
+	if (*stack && (*stack)->next)
+	{
+		if ((*stack)->n == 0)
+		{
+			fprintf(stderr, "L%d: division by zero\n", line_number);
+			free_stack(stack);
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			tmp = *stack;
+			*stack = (*stack)->next;
+			(*stack)->prev = NULL;
+			(*stack)->n /= tmp->n;
+			free(tmp);
+		}
+	}
+	else
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+}
